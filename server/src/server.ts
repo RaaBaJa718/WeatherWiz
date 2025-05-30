3098989931a16634e7b33f61a9a82302b7864d46
Ruben <ruben.alston646@gmail.com>
2025-05-29 23:18:44 -0400

    Add CORS support and update dependencies in package.json and package-lock.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import apiRouter from './routes/api/index.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors({
+  origin: 'https://weatherwiz-1-45ot.onrender.com'
+}));
 app.use(express.json());
 app.use('/api', apiRouter);
 
