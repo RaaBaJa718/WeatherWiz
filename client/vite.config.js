c1d96562ed2e426d582485a2f2024edf1ec77df8
Ruben <ruben.alston646@gmail.com>
2025-05-29 23:13:23 -0400

    Remove unnecessary comment and ensure preview configuration is properly defined in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
@@ -13,4 +12,7 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    allowedHosts: ['weatherwiz-1-45ot.onrender.com']
+  }
 });
